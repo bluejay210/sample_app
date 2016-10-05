2609b95f4fd19b23d73d82735ceefac8aee00d72
Andrea Kim <akim2100@gmail.com>
2016-10-05 16:46:44 -0400

    Another commit

--- web.py
+++ web.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/index_about')
+def index_about():
+    return render_template('index_about.html')
+
 app.run(debug=True)
